42d36cc0345a8a71d6278257dff7a34d3d96b2af
zayson <jymaeng95@gmail.com>
2022-06-27 11:59:56 +0900

    feat: 5강 주문 도메인 개발

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -9,6 +11,8 @@ import javax.persistence.*;
 @Entity
 @Getter
 @Setter
+// 기본 생성자를 막아줘야한다!
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
     @Id
     @GeneratedValue
@@ -27,4 +31,30 @@ public class OrderItem {
 
     private int count;
 
+    //== 생성 메서드 ==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        // 주문 들어온 만큼 재고를 줄여준다/
+        item.removeStock(count);
+
+        return orderItem;
+    }
+
+    //== 비즈니스 로직 ==//
+    public void cancel() {
+        //재고를 주문 수량만큼 다시 복구해준다.
+        getItem().addStock(count);
+    }
+
+    //== 조회 로직 ==/
+    /**
+     * 주문한 상품의 가격 * 개수
+     */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
